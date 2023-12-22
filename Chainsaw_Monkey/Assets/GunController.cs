7620b9143471cddb408f0d5ca020ec2c5abe532e
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2023-12-22 16:21:06 -0300

    Sistema de munição

--- Chainsaw_Monkey/Assets/GunController.cs
+++ Chainsaw_Monkey/Assets/GunController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 
 public class GunController : MonoBehaviour
 {
@@ -14,6 +15,12 @@ public class GunController : MonoBehaviour
     public float shootTimer = 0.8f;
     public float shootDuration = 0.8f;
     public bool shot = false;
+    public int magAmmo = 12;
+    public int remainingAmmo = 84;
+    public bool isReloading = false;
+    public float reloadTimer = 2;
+    public float reloadDuration = 2;
+    public TextMeshProUGUI ammoDisplay;
 
     void Start()
     {
@@ -33,15 +40,43 @@ public class GunController : MonoBehaviour
         
         if (Input.GetButtonDown("Fire1"))
         {
-            if(shootTimer >= shootDuration){
+            if(shootTimer >= shootDuration && magAmmo >0 && !isReloading){
                 Shoot();
                 shootTimer = 0;
                 shot = true;
+                magAmmo -= 1;
             }
-            
         }
+        if(Input.GetKeyDown(KeyCode.R) && magAmmo !=12 && remainingAmmo !=0){
+            isReloading = true;
+            int subtractedAmmo = 12 - magAmmo;
+            if(remainingAmmo < subtractedAmmo ){
+                magAmmo = remainingAmmo;
+                remainingAmmo = 0;
+            } 
+            else{
+                magAmmo = 12;
+                remainingAmmo -= subtractedAmmo;
+            }
+            reloadTimer = 0;
+        }
+
+        if(isReloading){
+            if(reloadTimer < reloadDuration){
+                Reload();
+            }
+            else {
+                reloadTimer = reloadDuration;
+                isReloading = false;
+            }
+        }
+
+        ammoDisplay.SetText($"{magAmmo} / {remainingAmmo}");
     }
 
+    void FixedUpdate(){
+        
+    }
     void Shoot()
     {
         particleSystem.Play();
@@ -65,5 +100,25 @@ public class GunController : MonoBehaviour
                 
             
         }
+    }
+
+    void Reload(){
+        Vector3 gunPos = transform.localPosition;
+        if(reloadTimer < reloadDuration/1.5f){
+            gunPos.y -= 3 * Time.deltaTime;
+            if(gunPos.y < -1){
+                gunPos. y = -1;
+            }
+        }
+        else{
+            gunPos.y += 3 * Time.deltaTime;
+            if(gunPos.y > -0.07f)
+            {
+                gunPos.y = -0.07f;
+            }
+        }
+        
+        transform.localPosition = gunPos;
+        reloadTimer += Time.deltaTime;
     }
 }
