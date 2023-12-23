86f672ed7a0344d4e6b98303dd3b9d6494898c59
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2023-12-22 23:15:03 -0300

    Final

--- Chainsaw_Monkey/Assets/AI/Hearing/CheckSoundNearby.cs
+++ Chainsaw_Monkey/Assets/AI/Hearing/CheckSoundNearby.cs
@@ -14,16 +14,16 @@ public class CheckSoundNearby : BehaviourTreeNode
             aIController.heardSound = false;
         }
         else if(aIController.playerScript.isRunning  
-        && Mathf.Abs(aIController.enemy.transform.position.x - aIController.player.transform.position.x) < 5
-        && Mathf.Abs(aIController.enemy.transform.position.y - aIController.player.transform.position.y) < 2f
-        && Mathf.Abs(aIController.enemy.transform.position.x - aIController.player.transform.position.x) < 5){
+        && Mathf.Abs(aIController.enemyHead.transform.position.x - aIController.player.transform.position.x) < 3
+        && Mathf.Abs(aIController.enemyHead.transform.position.y - aIController.player.transform.position.y) < 2f
+        && Mathf.Abs(aIController.enemyHead.transform.position.x - aIController.player.transform.position.x) < 3){
             aIController.soundPosition = aIController.player.transform.position;
             aIController.heardSound = true;
         }
         else if(!aIController.playerScript.isRunning  
-        && Mathf.Abs(aIController.enemy.transform.position.x - aIController.player.transform.position.x) < 2.5f
-        && Mathf.Abs(aIController.enemy.transform.position.y - aIController.player.transform.position.y) < 2f
-        && Mathf.Abs(aIController.enemy.transform.position.x - aIController.player.transform.position.x) < 2.5f){
+        && Mathf.Abs(aIController.enemyHead.transform.position.x - aIController.player.transform.position.x) < 2.5f
+        && Mathf.Abs(aIController.enemyHead.transform.position.y - aIController.player.transform.position.y) < 2f
+        && Mathf.Abs(aIController.enemyHead.transform.position.x - aIController.player.transform.position.x) < 2.5f){
             aIController.soundPosition = aIController.player.transform.position;
             aIController.heardSound = true;
         }
